585f8f4432fe3780291fd0c11e4e99818c0aaaad
Administrator <848406681@qq.com>
2022-06-09 00:06:23 +0800

    自动化测试框架

---
+++ api/__init__.py
@@ -0,0 +1,7 @@
+
+"""
+cacheout: 主要用作缓存
+1、导入：rrom import cacheout impot Cache
+2、设置缓存： cache.det(key,value)  ==> cache.set('name','张三')
+3、获取缓存 ：cache.get(key)  ==> cache.get('name)
+"""
---
+++ data/user_manager_data.py
@@ -0,0 +1,11 @@
+
+
+class UserManagerData():
+
+    add_user_data = {
+        "errno":0,
+        "username":"xuyinhui8",
+        "password":"123456",
+        "new_username":'xuyinhui9'
+
+    }
